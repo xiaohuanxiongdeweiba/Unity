7f9f8c5ea612e060a1b066ac431edb0a8c262ebb
yumengyang <yumengyang@fotoable.com>
2022-01-08 14:58:28 +0800

    交换糖果的移动

--- 3Match/Assets/Scripts/GameManager.cs
+++ 3Match/Assets/Scripts/GameManager.cs
@@ -53,6 +53,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public Sweet[,] Sweets
+    {
+        get
+        {
+            return sweets;
+        }
+
+        set
+        {
+            sweets = value;
+        }
+    }
+
     public int _yRow;
     public int _xColumn;
 
@@ -81,7 +94,7 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
-        sweets = new Sweet[_xColumn, _yRow];
+        Sweets = new Sweet[_xColumn, _yRow];
         CreatGrid();
         CreatSweet();
     }
@@ -89,44 +102,95 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ChoosenSweet();
         //选择
         if (Input.GetMouseButtonDown(0))
         {
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D raycastHit = Physics2D.Raycast(new Vector2(ray.origin.x, ray.origin.y), Vector2.down);
-            //RaycastHit raycastHit;
+            // raycastHit 是被选中的物体的collider
             if (raycastHit.collider)
             {
-                if (sweetChoosen == null)//选择对象
+                if (sweetChoosen == null)//选中的为空，则添加选择对象
                 {
                     GameObject ob = raycastHit.collider.gameObject;
-                    ob.GetComponentInChildren<SpriteRenderer>().color = Color.red;
-                    print(ob.name);
+                    //ob.GetComponentInChildren<SpriteRenderer>().color = Color.red;
+                    //print(ob.name);
                     sweetChoosen = ob.GetComponent<Collider2D>();
                 }
-                else if (sweetChoosen == raycastHit.collider.gameObject.GetComponent<Collider2D>())//取消选择
+                else//选择的不为空
                 {
+                    float scx = sweetChoosen.gameObject.GetComponent<Sweet>().XPos;
+                    float scy = sweetChoosen.gameObject.GetComponent<Sweet>().YPos;
+                    float sex = raycastHit.collider.gameObject.GetComponent<Sweet>().XPos;
+                    float sey = raycastHit.collider.gameObject.GetComponent<Sweet>().YPos;
+                    if (sweetChoosen == raycastHit.collider.gameObject.GetComponent<Collider2D>())//第二次点击的同一个目标，取消选择
+                    {
 
-                    GameObject ob = raycastHit.collider.gameObject;
-                    ob.GetComponentInChildren<SpriteRenderer>().color = Color.white;
-                    sweetChoosen = null;
-                }
-                else if()//选择第二个对象
-                {
-                    sweetChoosen.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.white;
-                    GameObject ob = raycastHit.collider.gameObject;
-                    ob.GetComponentInChildren<SpriteRenderer>().color = Color.red;
-                    print(ob.name);
-                    sweetChoosen = ob.GetComponent<Collider2D>();
-                }
+                        //GameObject ob = raycastHit.collider.gameObject;
+                        //ob.GetComponentInChildren<SpriteRenderer>().color = Color.white;
+                        sweetChoosen = null;
+                    }
+                    else if (((scx == sex) && (scy - 1 == sey || scy + 1 == sey)) || ((scy == sey) && (scx - 1 == sex || scx + 1 == sex)))//第二个选择的能交换
+                    {
+                        //进行位置交换
+                        print("相邻");
+                        //sweetChoosen目标位置为pos[0]
+                        Vector3[] pos = sweetChoosen.gameObject.GetComponent<Move>().ExchangeSweet(sweetChoosen, raycastHit.collider);
+                        //sweetChoosen.gameObject.GetComponent<Transform>().Translate(new Vector3 ((pos[0].x - sweetChoosen.gameObject.GetComponent<Transform>().position.x)*10f, (pos[0].y - sweetChoosen.gameObject.GetComponent<Transform>().position.y))*10f);
+                        //raycastHit.collider.gameObject.GetComponent<Transform>().Translate(new Vector3( pos[1].x - raycastHit.collider.gameObject.GetComponent<Transform>().position.x, pos[1].y - raycastHit.collider.gameObject.GetComponent<Transform>().position.y));
+                        sweetChoosen.gameObject.GetComponent<Move>().DestPos = pos[0];
+                        raycastHit.collider.gameObject.GetComponent<Move>().DestPos = pos[1];
+                        sweetChoosen.gameObject.GetComponent<Move>().IsMove = true;
+                        raycastHit.collider.gameObject.GetComponent<Move>().IsMove = true;
+                        //sweetChoosen.gameObject.GetComponent<Transform>().position = pos[0];
+                        //raycastHit.collider.gameObject.GetComponent<Transform>().position = pos[1];
+                        sweetChoosen = null;
+                        print("交换成功");
 
+                    }
+                    else//第二个选择的为不相关，变换被选中的目标
+                    {
+                        GameObject ob = raycastHit.collider.gameObject;
+                        sweetChoosen = ob.GetComponent<Collider2D>();
+                    }
+                }
 
             }
 
 
 
         }
+    }
+
+    public void ChoosenSweet()
+    {
+        for (int i = 0; i < _xColumn; i++)
+        {
+            for (int j = 0; j < _yRow; j++)
+            {
+                if (sweetChoosen == Sweets[i, j].GetComponent<Collider2D>())
+                {
+                    sweetChoosen.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.red;
+                }
+                else if (Sweets[i, j].GetComponent<Collider2D>() != sweetChoosen)
+                {
+                    Sweets[i, j].GetComponentInChildren<SpriteRenderer>().color = Color.white;
+                }
+            }
+
+        }
+        if (sweetChoosen != null)
+        {
+            print(sweetChoosen.gameObject.GetComponent<Sweet>().XPos + "," + sweetChoosen.gameObject.GetComponent<Sweet>().YPos);
+
+        }
+        else
+        {
+            print("没有选中");
+        }
+
     }
     /// <summary>
     /// 生成巧克力网格
@@ -164,37 +228,37 @@ public class GameManager : MonoBehaviour
             for (int j = 0; j < _yRow; j++)
             {
                 GameObject newSweet = Instantiate(sweetPrefabDic[SweetType.NORMAL], new Vector3(i, j), Quaternion.identity, Instance.transform);
-                sweets[i, j] = newSweet.GetComponent<Sweet>();
+                Sweets[i, j] = newSweet.GetComponent<Sweet>();
                 //设置随机颜色
 
                 int colorNum = random.Next(0, 6);
-                SpriteRenderer sp = sweets[i, j].GetComponentInChildren<SpriteRenderer>();
+                SpriteRenderer sp = Sweets[i, j].GetComponentInChildren<SpriteRenderer>();
                 //SpriteRenderer sp = newSweet.GetComponentInChildren<SpriteRenderer>();
                 switch (colorNum)
                 {
                     case 0:
                         sp.sprite = sweetSpriteDic[SweetColor.RED];
-                        sweets[i, j].IniSweet(i, j, SweetColor.RED, SweetType.NORMAL);
+                        Sweets[i, j].IniSweet(i, j, SweetColor.RED, SweetType.NORMAL);
                         break;
                     case 1:
                         sp.sprite = sweetSpriteDic[SweetColor.YELLOW];
-                        sweets[i, j].IniSweet(i, j, SweetColor.YELLOW, SweetType.NORMAL);
+                        Sweets[i, j].IniSweet(i, j, SweetColor.YELLOW, SweetType.NORMAL);
                         break;
                     case 2:
                         sp.sprite = sweetSpriteDic[SweetColor.GREAN];
-                        sweets[i, j].IniSweet(i, j, SweetColor.GREAN, SweetType.NORMAL);
+                        Sweets[i, j].IniSweet(i, j, SweetColor.GREAN, SweetType.NORMAL);
                         break;
                     case 3:
                         sp.sprite = sweetSpriteDic[SweetColor.BLUE];
-                        sweets[i, j].IniSweet(i, j, SweetColor.BLUE, SweetType.NORMAL);
+                        Sweets[i, j].IniSweet(i, j, SweetColor.BLUE, SweetType.NORMAL);
                         break;
                     case 4:
                         sp.sprite = sweetSpriteDic[SweetColor.PURPLE];
-                        sweets[i, j].IniSweet(i, j, SweetColor.PURPLE, SweetType.NORMAL);
+                        Sweets[i, j].IniSweet(i, j, SweetColor.PURPLE, SweetType.NORMAL);
                         break;
                     case 5:
                         sp.sprite = sweetSpriteDic[SweetColor.PINK];
-                        sweets[i, j].IniSweet(i, j, SweetColor.PINK, SweetType.NORMAL);
+                        Sweets[i, j].IniSweet(i, j, SweetColor.PINK, SweetType.NORMAL);
                         break;
                 }
                 CorrectPosition(newSweet);
--- 3Match/Assets/Scripts/Move.cs
+++ 3Match/Assets/Scripts/Move.cs
@@ -4,7 +4,37 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    
+    private bool isMove = false;
+    private Vector3 destPos;
+
+
+    public bool IsMove
+    {
+        get
+        {
+            return isMove;
+        }
+
+        set
+        {
+            isMove = value;
+        }
+    }
+
+    public Vector3 DestPos
+    {
+        get
+        {
+            return destPos;
+        }
+
+        set
+        {
+            destPos = value;
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,19 +44,45 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (IsMove)
+        {
+        gameObject.GetComponent<Transform>().Translate((DestPos-gameObject.GetComponent<Transform>().position)*0.1f);
+            if (gameObject.GetComponent<Transform>().position == destPos)
+            {
 
+                this.IsMove = false;
+            }
+        }
     }
-    public void ExchangeSweet( Collider2D sweetSelect, Collider2D sweetExchange)
+    public Vector3[] ExchangeSweet(Collider2D sweetSelect, Collider2D sweetExchange)
     {
-        Vector3 sweet1;
-        Vector3 sweet2;
+        Vector3 sweet1;//1糖原来位置
+        Vector3 sweet2 = new Vector3(0, 0, 0);//2糖原来位置
         Vector3 temp;
-        sweet1= sweetSelect.gameObject.GetComponent<Transform>().transform.position;
+        sweet1 = sweetSelect.gameObject.GetComponent<Transform>().transform.position;
         sweet2 = sweetExchange.gameObject.GetComponent<Transform>().transform.position;
         temp = sweet1;
-        sweet1 = sweet2;
-        sweet2 = temp;
+        sweet1 = sweet2;//1糖交换后位置
+        sweet2 = temp;//2糖交换后位置
+        Vector3[] pos = new Vector3[2];
+        pos[0] = sweet1;
+        pos[1] = sweet2;
+        //位置交换，在sweets 里面的 放置也应该交换，sweet 里面记录的 坐标也应该交换
+
+        //sweets里面的放置交换
+
+        int[] s1p = new int[] { sweetSelect.gameObject.GetComponent<Sweet>().XPos, sweetSelect.gameObject.GetComponent<Sweet>().YPos };
+        int[] s2p = new int[] { sweetExchange.gameObject.GetComponent<Sweet>().XPos, sweetExchange.gameObject.GetComponent<Sweet>().YPos };
+        //找到sweet里面记录的坐标
+        sweetSelect.gameObject.GetComponent<Sweet>().XPos = s2p[0];
+        sweetSelect.gameObject.GetComponent<Sweet>().YPos = s2p[1];
+        sweetExchange.gameObject.GetComponent<Sweet>().XPos = s1p[0];
+        sweetExchange.gameObject.GetComponent<Sweet>().YPos = s1p[1];
+        GameManager.Instance.Sweets[s1p[0], s1p[1]] = sweetExchange.gameObject.GetComponent<Sweet>();
+        GameManager.Instance.Sweets[s2p[0], s2p[1]] = sweetSelect.gameObject.GetComponent<Sweet>();
+        return pos;
+
     }
 
-    
+
 }
